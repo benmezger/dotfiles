98abb6d4f4805a860cb9bc4f3e2e043c887a1988
Ben Mezger <me@benmezger.nl>
2016-02-17 21:03:14 -0200

    Fixed path name

--- bootstrap/git_packages.sh
+++ bootstrap/git_packages.sh
@@ -1,8 +1,8 @@
 echo "Cloning powerline fonts."
-git clone https://github.com/Lokaltog/powerline-fonts.git ~/Workspace/git/
+git clone https://github.com/Lokaltog/powerline-fonts.git ~/workspace/git/
 
 echo "\nCloning tmux-plugin manager"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "\nCloning YCMD"
-git clone git@github.com:Valloric/ycmd.git ~/Workspace/git/ycmd
+git clone git@github.com:Valloric/ycmd.git ~/workspace/git/ycmd
