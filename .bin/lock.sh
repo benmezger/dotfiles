8fff6e2cabccb39a6fc1321c5fe9c1c46abea309
Ben Mezger <me@benmezger.nl>
2016-03-08 09:11:07 -0300

    Fixed scripts and screen-lock

--- .bin/lock.sh
+++ .bin/lock.sh
@@ -1,4 +1,4 @@
-scrot -e 'convert -resize 20% -fill "#282828" -colorize 50% -blur 0x1 -resize 500% $f ~/Pictures/lockbg.png'
-convert -gravity center -composite ~/Pictures/lockbg.png ~/Pictures/Icons/lock.png ~/Pictures/lockfinal.png
-i3lock -u -i ~/Pictures/lockfinal.png
-rm ~/Pictures/lockfinal.png ~/Pictures/lockbg.png
+#!/bin/bash
+scrot /tmp/screenshot.png
+convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+i3lock -i /tmp/screenshotblur.png
