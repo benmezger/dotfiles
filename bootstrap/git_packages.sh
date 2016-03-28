4aee78442f8be70e181988dea50d4bbcc8091784
Ben Mezger <me@benmezger.nl>
2016-03-28 16:02:48 -0300

    Fixed git url for YCMD

--- bootstrap/git_packages.sh
+++ bootstrap/git_packages.sh
@@ -5,4 +5,4 @@ echo "\nCloning tmux-plugin manager"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "\nCloning YCMD"
-git clone git@github.com:Valloric/ycmd.git ~/workspace/git/ycmd
+git clone https://github.com/Valloric/ycmd.git ~/workspace/git/ycmd
