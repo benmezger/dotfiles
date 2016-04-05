ef655b49d5d2d08cdead02fa6b861e37c364b9c5
Ben Mezger <me@benmezger.nl>
2016-04-05 09:20:41 -0300

    Fixed and removed unwanted functions

--- .zsh/funcs.zsh
+++ .zsh/funcs.zsh
@@ -16,12 +16,6 @@ ipynbk(){
     kill $(ps aux | grep '[i]python notebook' | awk '{print $2}')
 }
 
-# pdpid=$(pgrep -f powerline-daemon)
-# pdpid=$(ps ax | grep powerline-daemon | cut -d":" -f1 | cut -d" " -f1 | head -n 1)
-# if [ -z $pdpid ]; then
-#     powerline-daemon -q&
-# fi
-
 take(){
 	mkdir -p $1
 	cd $1
@@ -30,26 +24,8 @@ take(){
 function copyfile {
 	[[ "$#" != 1 ]] && return 1
 	local file_to_copy=$1
-	cat $file_to_copy | pbcopy
+	cat $file_to_copy | xsel --clipboard
 }
-
-# sources
-if [ -d "$HOME/.zsh/z" ]; then
-	source $HOME/.zsh/z/z.sh # z for cd
-fi
-
-if [ -d "$HOME/.zsh/s" ]; then
-	source $HOME/.zsh/s/s.sh # s is a tool for ssh like z for cd
-fi
-
-if [ -d "$HOME/.zsh/bd/" ]; then
-	source $HOME/.zsh/bd/bd.zsh # Quickly go back to a specific parent directory instead of typing cd ../../.. redundantly
-fi
-
-if [ -z "$SSH_AUTH_SOCK" ] ; then
-    eval `ssh-agent -s`
-    ssh-add ~/.ssh/bdigi_ocean
-fi
 
 # Create a new directory and enter it
 function mkd() {
