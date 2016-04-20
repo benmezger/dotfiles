fc3034e3923ecd203d7bad1888af7ddc24e30578
Ben Mezger <me@benmezger.nl>
2016-04-20 09:09:28 -0300

    A better and nicer installer

--- bootstrap/git_packages.sh
+++ bootstrap/git_packages.sh
@@ -2,5 +2,6 @@
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/Valloric/ycmd.git ~/workspace/git/packages/
+(cd ~/workspace/git/packages; git submodule update --init --recursive)
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-git clone https://github.com/baskerville/sxhkd.git ~/workspace/git/packages/
+git clone https://github.com/chriskempson/base16-shell.git ~/workspace/git/
