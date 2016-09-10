8f51e0160f6d65c806870ae80b0711873f08373c
Ben Mezger <me@benmezger.nl>
2016-09-10 18:47:23 -0300

    Emacs config refactoring

--- .emacs.d/site-lisp/setup-acejump.el
+++
@@ -1,5 +0,0 @@
-;; ace-jump-mode
-
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
-
-(provide 'setup-acejump)
--- .emacs.d/site-lisp/setup-theme.el
+++
@@ -1,9 +0,0 @@
-;; monokai-theme
-
-(if (display-graphic-p)
-    (progn
-      (scroll-bar-mode 0)))
-
-(load-theme 'monokai t)
-
-(provide 'setup-theme)
