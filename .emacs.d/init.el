156fcdff90411d319d02494e05d4c206eac24f7e
Ben Mezger <me@benmezger.nl>
2016-09-10 18:48:07 -0300

    New emacs config (basic)

---
+++ .emacs.d/init.el
@@ -0,0 +1,25 @@
+;; let cask manage my packages
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+;; custom site lisp
+(setq site-lisp-dir
+       (expand-file-name "site-lisp" user-emacs-directory))
+(add-to-list 'load-path site-lisp-dir)
+
+;; keep emacs custom settings in a different file.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+;; keep emacs configuration in a different file
+(setq emacs-file (expand-file-name "emacs.el" user-emacs-directory))
+(load emacs-file)
+
+;; load site-lisps
+(require 'setup-completion)
+(require 'setup-orgmode)
+(require 'setup-editorconfig)
+(require 'setup-magit)
+(require 'setup-gitgutter)
+(require 'setup-smartline)
+(require 'setup-smartparens)
