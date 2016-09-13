64975fb53b393f5950e6fe3458f6a5b22d30f9f0
Ben Mezger <me@benmezger.nl>
2016-09-13 01:30:46 -0300

    Exec-path-from-shell enabled

---
+++ .emacs.d/site-lisp/setup-exec-path-from-shell.el
@@ -0,0 +1,10 @@
+;; exec-path-from-shell
+(require 'exec-path-from-shell)
+
+; (setq exec-path-from-shell-arguments '("-l"))
+
+(when (memq window-system '(mac ns))
+   (exec-path-from-shell-initialize)
+     (exec-path-from-shell-copy-env "GPG_AGENT_INFO"))
+
+(provide 'setup-exec-path-from-shell)
