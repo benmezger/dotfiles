49b54a6d96ce534e8bc9ec1f6fb3615cacc86c4e
Ben Mezger <me@benmezger.nl>
2016-09-29 18:29:26 -0300

    New org files path

--- .emacs.d/site-lisp/setup-orgmode.el
+++ .emacs.d/site-lisp/setup-orgmode.el
@@ -12,8 +12,8 @@
 (setq org-log-done t)
 
 ;; org-mode directory
-(setq org-directory "~/documents/org")
-(setq org-agenda-files '("~/documents/org"))
+(setq org-directory "~/orgs")
+(setq org-agenda-files '("~/orgs"))
 (setq org-src-fontify-natively t)
 
 ;; indent hook
@@ -23,7 +23,7 @@
           t)
 
 ; quick notes
-(setq org-default-notes-file (expand-file-name "~/documents/org/refile.org"))
+(setq org-default-notes-file (expand-file-name "~/orgs/refile.org"))
 
 ;; custom todo-keywords
 (setq org-todo-keywords
@@ -50,21 +50,21 @@
 
 ;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
 (setq org-capture-templates
-      (quote (("t" "todo" entry (file "~/documents/org/refile.org")
+      (quote (("t" "todo" entry (file "~/orgs/refile.org")
                "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
-              ("r" "respond" entry (file "~/documents/org/refile.org")
+              ("r" "respond" entry (file "~/orgs/refile.org")
                "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
-              ("n" "note" entry (file "~/documents/org/refile.org")
+              ("n" "note" entry (file "~/orgs/refile.org")
                "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-              ("j" "Journal" entry (file+datetree "~/documents/org/diary.org")
+              ("j" "Journal" entry (file+datetree "~/orgs/diary.org")
                "* %?\n%U\n" :clock-in t :clock-resume t)
-              ("w" "org-protocol" entry (file "~/documents/org/refile.org")
+              ("w" "org-protocol" entry (file "~/orgs/refile.org")
                "* TODO Review %c\n%U\n" :immediate-finish t)
-              ("m" "Meeting" entry (file "~/documents/org/refile.org")
+              ("m" "Meeting" entry (file "~/orgs/refile.org")
                "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-              ("p" "Phone call" entry (file "~/documents/org/refile.org")
+              ("p" "Phone call" entry (file "~/orgs/refile.org")
                "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
-              ("h" "Habit" entry (file "~/documents/org/refile.org")
+              ("h" "Habit" entry (file "~/orgs/refile.org")
                "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
 
 ;; Remove empty LOGBOOK drawers on clock out
