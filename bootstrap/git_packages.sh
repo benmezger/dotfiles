90699f33c98f7a85ebcd84ae7b28d6cb203bff39
Ben Mezger <me@benmezger.nl>
2017-04-15 11:36:06 -0300

    Moving bootstrap to Ansible

--- bootstrap/git_packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/Valloric/ycmd.git ~/workspace/git/packages/
-(cd ~/workspace/git/packages; git submodule update --init --recursive)
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-git clone https://github.com/chriskempson/base16-shell.git ~/workspace/git/
