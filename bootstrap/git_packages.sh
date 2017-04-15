963ded97040c89d87424dd5225b701decbe1f582
Ben Mezger <me@benmezger.nl>
2017-04-14 23:37:35 -0300

    Don't do things my hand, replace this with ansible's playbook

--- bootstrap/git_packages.sh
+++
@@ -1,8 +0,0 @@
-echo "Cloning powerline fonts."
-git clone https://github.com/Lokaltog/powerline-fonts.git ~/workspace/git/
-
-echo "\nCloning tmux-plugin manager"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-echo "\nCloning YCMD"
-git clone https://github.com/Valloric/ycmd.git ~/workspace/git/ycmd
