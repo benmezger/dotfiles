d42354b866154a44525aad9cda603e2092fdca25
Ben Mezger <me@benmezger.nl>
2017-05-02 12:18:29 -0300

    Package.el automatic config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,11 @@
 ;; let cask manage my packages
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
+
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
