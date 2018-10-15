257f5ea0bf61926dc1db1deb840ae6f7b477ef49
Ben Mezger <me@benmezger.nl>
2018-10-15 17:41:59 -0300

    Fish: Alias to nvim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -71,6 +71,7 @@ alias lt='ll -tr'        # Lists sorted by date, most recent last.
 alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
 alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
 alias sl='ls' # I often screw this up.
+alias vim="nvim"
 
 # fasd aliases
 alias a='fasd -a'        # any
