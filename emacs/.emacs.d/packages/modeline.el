bfabdc0b5b7b77038dd5817b0332c339d7b91494
Ben Mezger <me@benmezger.nl>
2019-04-27 23:06:51 -0300

    Emacs: set no-config-load-theme for modeline and workspace variable

--- emacs/.emacs.d/packages/modeline.el
+++ emacs/.emacs.d/packages/modeline.el
@@ -1,8 +1,10 @@
-(use-package smart-mode-line-atom-one-dark-theme
-  :ensure t)
+;; (use-package smart-mode-line-atom-one-dark-theme
+;; :ensure t)
 
 (use-package smart-mode-line
   :ensure t
   :config
   ;; (setq sml/theme 'atom-one-dark)
+  (setq sml/no-confirm-load-theme t)
+  (add-to-list 'sml/replacer-regexp-list '("^~/workspace/" " :WP: ") t)
   (sml/setup))
