b3caf5ff99fd5409da2328878e603fec7bc70038
Ben Mezger <me@benmezger.nl>
2019-05-01 13:53:12 -0300

    Emacs: Set org-mode keybindings and default note file

--- emacs/.emacs.d/packages/orgmode.el
+++ emacs/.emacs.d/packages/orgmode.el
@@ -4,10 +4,12 @@
   :ensure t
   :mode ("\\.org\\'" . org-mode)
   :bind (("C-c l" . org-store-link)
-         ("C-c a" . org-agenda))
+         ("C-c a" . org-agenda)
+         ("C-c c" . org-capture))
   :config
   (progn
-    (setq org-directory "~/org")
+    (setq org-directory "~/orgs")
+    (setq org-default-notes-file "~/orgs/todos.org")
     (setq org-log-done t)
     (setq org-todo-keywords
           '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))))
