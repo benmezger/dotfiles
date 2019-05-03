5c11717cf0c116dbe54b942648840c3f5875642e
Ben Mezger <me@benmezger.nl>
2019-05-03 00:31:27 -0300

    Emacs: Fixed helm.el indentation

--- emacs/.emacs.d/packages/helm.el
+++ emacs/.emacs.d/packages/helm.el
@@ -1,18 +1,18 @@
 ;; helm mode
 (use-package helm
   :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ("C-x f" . helm-recentf)
-         ("M-y" . helm-show-kill-ring)
-         ("C-x b" . helm-mini)
-         ("C-c h o" . helm-occur))
+          ("C-x C-f" . helm-find-files)
+          ("C-x f" . helm-recentf)
+          ("M-y" . helm-show-kill-ring)
+          ("C-x b" . helm-mini)
+          ("C-c h o" . helm-occur))
   :bind (:map helm-map
-              ("M-i" . helm-previous-line)
-              ("M-k" . helm-next-line)
-              ("M-I" . helm-previous-page)
-              ("M-K" . helm-next-page)
-              ("M-h" . helm-beginning-of-buffer)
-              ("M-H" . helm-end-of-buffer))
+          ("M-i" . helm-previous-line)
+          ("M-k" . helm-next-line)
+          ("M-I" . helm-previous-page)
+          ("M-K" . helm-next-page)
+          ("M-h" . helm-beginning-of-buffer)
+          ("M-H" . helm-end-of-buffer))
   :ensure t
   :config (progn
             (setq helm-buffers-fuzzy-matching t)
