76796bac7a06ed6680dc8a6edbb36c05a3f92237
Ben Mezger <me@benmezger.nl>
2019-05-08 14:25:30 -0300

    Emacs: Set exec-path-froms-shell variables to copy

--- emacs/.emacs.d/packages/execpath.el
+++ emacs/.emacs.d/packages/execpath.el
@@ -1,4 +1,7 @@
 (use-package exec-path-from-shell
-  :if (memq window-system '(mac ns))
+  :if (memq window-system '(mac ns x))
   :ensure t
-  :config (exec-path-from-shell-initialize))
+  :config
+  (exec-path-from-shell-copy-env "LANG")
+  (exec-path-from-shell-copy-env "MAKEFLAGS")
+  (exec-path-from-shell-initialize))
