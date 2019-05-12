e4ec767f00ba584e88b490944dbadc229aa63dc5
Ben Mezger <me@benmezger.nl>
2019-05-12 13:13:30 -0300

    Zsh: Fetch autosuggestion asynchronously

--- zsh/.zsh/init.zsh
+++ zsh/.zsh/init.zsh
@@ -55,3 +55,6 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
     eval "$(pyenv init -)"
 fi
+
+# fetch suggestions asynchronously
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
