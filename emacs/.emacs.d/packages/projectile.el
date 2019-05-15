16dfdcb924b02360b9f321f48fe311c7852d3993
Ben Mezger <me@benmezger.nl>
2019-05-15 18:31:37 -0300

    Emacs: Projectile indexing and caching config.

--- emacs/.emacs.d/packages/projectile.el
+++ emacs/.emacs.d/packages/projectile.el
@@ -3,4 +3,7 @@
   :defer t
   :bind (("C-c p" . projectile-command-map))
   :init
+  (setq projectile-indexing-method 'native)
+  (setq projectile-enable-caching t)
+  (setq projectile-project-search-path '("~/workspace/"))
   (projectile-mode 1))
