4941cf4ed4bdde3fce4e6a4ecf9fe1580f58f572
Ben Mezger <me@benmezger.nl>
2019-05-17 17:09:57 -0300

    Emacs: Swith python version on projectile change

--- emacs/.emacs.d/packages/projectile.el
+++ emacs/.emacs.d/packages/projectile.el
@@ -7,4 +7,14 @@
   (setq projectile-enable-caching t)
   (setq projectile-project-search-path '("~/workspace/"))
   ;; (setq projectile-completion-system 'helm)
-  (projectile-mode 1))
+  (projectile-mode 1)
+
+  ;; switch python version together with current project
+  (require 'pyenv-mode)
+  (defun projectile-pyenv-mode-set ()
+    "Set pyenv version matching project name."
+    (let ((project (projectile-project-name)))
+      (if (member project (pyenv-mode-versions))
+        (pyenv-mode-set project)
+        (pyenv-mode-unset))))
+  (add-hook 'projectile-after-switch-project-hook 'projectile-pyenv-mode-set))
