e217cd93224a8764fbca1f7bbfee8c0ed10bf9d7
Ben Mezger <me@benmezger.nl>
2019-05-20 18:40:43 -0300

    Emacs: Make evil-mode drop to normal state after 30sec of idle

--- emacs/.emacs.d/packages/evil.el
+++ emacs/.emacs.d/packages/evil.el
@@ -50,6 +50,19 @@
   (add-hook 'after-save-hook #'evil-normal-state)
   :config
   (evil-mode)
+  (defun evil-normalize-all-buffers ()
+    "Force a drop to normal state."
+    (unless (eq evil-state 'normal)
+      (dolist (buffer (buffer-list))
+        (set-buffer buffer)
+        (unless (or (minibufferp)
+                  (eq evil-state 'emacs))
+          (evil-force-normal-state)))
+      (message "Dropped back to normal state in all buffers")))
+
+  (defvar evil-normal-timer
+    (run-with-idle-timer 30 t #'evil-normalize-all-buffers)
+    "Drop back to normal state after idle for 30 seconds.")
 
   ;; vim-like keybindings everywhere in emacs
   (use-package evil-collection
