3f24d26de6624954ab5805f00f8ed59d395670f9
Ben Mezger <me@benmezger.nl>
2019-05-30 00:23:50 -0300

    Zsh: Fix OSX ls alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -15,7 +15,7 @@ if (( ${+commands[dircolors]} )); then
     else
         export LS_COLORS='di=1;34:ln=35:so=32:pi=33:ex=31:bd=1;36:cd=1;33:su=30;41:sg=30;46:tw=30;42:ow=30;43'
     fi
-    alias ls='ls --group-directories-first --color=auto'
+    alias ls='ls -aFhlG'
 else
     # BSD
     (( ! ${+CLICOLOR} )) && export CLICOLOR=1
