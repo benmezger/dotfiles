57615c62d05086317d38e38a73779c8c622f9a08
Ben Mezger <me@benmezger.nl>
2019-07-12 18:55:57 -0300

    Emacs: Automatically close all Magit buffers on magit-quit

--- emacs/.emacs.d/packages/magit.el
+++ emacs/.emacs.d/packages/magit.el
@@ -6,4 +6,13 @@
   (require 'evil-magit)
   (setq magit-completing-read-function 'ivy-completing-read)
 
+  (defun mu-magit-kill-buffers ()
+    "Restore window configuration and kill all Magit buffers."
+    (interactive)
+    (let ((buffers (magit-mode-get-buffers)))
+      (magit-restore-window-configuration)
+      (mapc #'kill-buffer buffers)))
+
+  (bind-key "q" #'mu-magit-kill-buffers magit-status-mode-map)
+
   :bind (("C-x g" . magit-status)))
