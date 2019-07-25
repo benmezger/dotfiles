f790f6695bc50dd861b6efe8d8b53dc113917ae2
Ben Mezger <me@benmezger.nl>
2019-07-25 19:14:10 -0300

    Emacs: Fix undo-tree history directory

--- emacs/.emacs.d/packages/undotree.el
+++ emacs/.emacs.d/packages/undotree.el
@@ -6,8 +6,6 @@
     (global-undo-tree-mode)
     (setq undo-tree-visualizer-timestamps t)
     (setq undo-tree-visualizer-diff t)
-    (setq undo-tree-auto-save-history t
-      undo-tree-history-directory-alist
-      `(("." . ,(concat emacs-cache-directory "undo"))))
-    (unless (file-exists-p (concat emacs-cache-directory "undo"))
-      (make-directory (concat emacs-cache-directory "undo")))))
+    (setq undo-tree-auto-save-history t)
+    (setq undo-tree-history-directory-alist
+      `(("." . ,(concat emacs-cache-directory "undo"))))))
