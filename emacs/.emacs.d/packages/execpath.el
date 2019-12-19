07c76f20d55c865c194b449af2d52d880a28e8af
Ben Mezger <me@benmezger.nl>
2019-12-19 11:20:06 -0300

    Emacs: Check if emacs is running under osx before using execpath

--- emacs/.emacs.d/packages/execpath.el
+++ emacs/.emacs.d/packages/execpath.el
@@ -1,8 +1,9 @@
-(use-package exec-path-from-shell
-  :ensure t
-  :if (memq window-system '(mac ns x))
-  :config
-  (exec-path-from-shell-copy-env "PATH")
-  (exec-path-from-shell-copy-env "LANG")
-  (exec-path-from-shell-copy-env "MAKEFLAGS")
-  (exec-path-from-shell-initialize))
+(if (eq system-type 'darwin)
+  (use-package exec-path-from-shell
+    :ensure t
+    :if (memq window-system '(mac ns x))
+    :config
+    (exec-path-from-shell-copy-env "PATH")
+    (exec-path-from-shell-copy-env "LANG")
+    (exec-path-from-shell-copy-env "MAKEFLAGS")
+    (exec-path-from-shell-initialize)))
