b7c10b77e66d1e05fdd85c2782591999481e84fa
Ben Mezger <me@benmezger.nl>
2019-12-23 16:55:07 -0300

    Emacs: Ignore check startup files for exec-path-from-shell

--- emacs/.emacs.d/packages/execpath.el
+++ emacs/.emacs.d/packages/execpath.el
@@ -3,6 +3,7 @@
     :ensure t
     :if (memq window-system '(mac ns x))
     :config
+    (setq exec-path-from-shell-check-startup-files nil)
     (exec-path-from-shell-copy-env "PATH")
     (exec-path-from-shell-copy-env "LANG")
     (exec-path-from-shell-copy-env "MAKEFLAGS")
