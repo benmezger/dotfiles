0567c1a3bcb9885153e4a6216cb8a57e111e4f89
Ben Mezger <me@benmezger.nl>
2020-02-03 15:57:30 -0300

    Emacs-doom: Add initial Emacs-Doom configuration

---
+++ doom-emacs/.doom.d/config.el
@@ -0,0 +1,189 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Ben Mezger"
+      user-mail-address "me@benmezger.nl")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "Fira Code" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-gruvbox)
+
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org/")
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type t)
+
+(display-time-mode 1)
+(display-battery-mode 1)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+;;
+
+(after! lsp-mode
+  :config
+  (setq lsp-response-timeout 90000))
+
+(after! company-lsp
+  :config
+  (push 'company-lsp company-backends)
+  (setq company-lsp-async t)
+  (setq company-lsp-cache-candidates t)
+  (setq company-lsp-enable-recompletion t))
+
+(after! company
+  :config
+  (setq company-selection-wrap-around t)
+  (setq company-minimum-prefix-length 1)
+  (setq company-idle-delay 0))
+
+(after! doom-modeline
+  :config
+  (setq doom-modeline-height 20)
+  (setq doom-modeline-buffer-file-name-style 'truncate-upto-project)
+
+  (setq doom-modeline-icon (display-graphic-p))
+  (setq doom-modeline-major-mode-icon t)
+  (setq doom-modeline-major-mode-color-icon t)
+
+  (setq doom-modeline-buffer-state-icon t)
+  (setq doom-modeline-buffer-modification-icon t)
+
+  (setq doom-modeline-enable-word-count t)
+  (setq doom-modeline-buffer-encoding t)
+  (setq doom-modeline-indent-info t)
+
+  (setq doom-modeline-mu4e nil)
+  (setq doom-modeline-irc nil)
+
+  (setq doom-modeline-env-version t)
+  (setq doom-modeline-env-load-string "...")
+
+  (custom-set-faces
+    '(mode-line ((t (:height 1))))
+    '(mode-line-inactive ((t (:height 1)))))
+
+  ;; Don’t compact font caches during GC.
+  (setq inhibit-compacting-font-caches t))
+
+(after! evil
+  :init
+  (add-hook 'after-save-hook #'evil-normal-state)
+  :config
+
+  (defun evil-normalize-all-buffers ()
+    "Force a drop to normal state."
+    (unless (eq evil-state 'normal)
+      (dolist (buffer (buffer-list))
+        (set-buffer buffer)
+        (unless (or (minibufferp)
+                  (eq evil-state 'emacs))
+          (evil-force-normal-state)))))
+
+  (defvar evil-normal-timer
+    (run-with-idle-timer 120 t #'evil-normalize-all-buffers)
+    "Drop back to normal state after idle for 30 seconds."))
+
+(after! flycheck
+  :config
+  (setq flycheck-check-syntax-automatically '(save mode-enable)))
+
+(after! counsel
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+
+  ;; enable this if you want `swiper' to use it
+  (setq search-default-mode #'char-fold-to-regexp)
+  (setq ivy-re-builders-alist
+        '((swiper . ivy--regex-plus)
+           (counsel-rg . ivy--regex-plus)
+          (t      . ivy--regex-fuzzy)))
+
+  (recentf-mode 1)
+  (defun eh-ivy-return-recentf-index (dir)
+    (when (and (boundp 'recentf-list)
+            recentf-list)
+      (let ((files-list
+              (cl-subseq recentf-list
+                0 (min (- (length recentf-list) 1) 20)))
+             (index 0))
+        (while files-list
+          (if (string-match-p dir (car files-list))
+            (setq files-list nil)
+            (setq index (+ index 1))
+            (setq files-list (cdr files-list))))
+        index)))
+
+  (defun eh-ivy-sort-file-function (x y)
+    (let* ((x (concat ivy--directory x))
+            (y (concat ivy--directory y))
+            (x-mtime (nth 5 (file-attributes x)))
+            (y-mtime (nth 5 (file-attributes y))))
+      (if (file-directory-p x)
+        (if (file-directory-p y)
+          (let ((x-recentf-index (eh-ivy-return-recentf-index x))
+                 (y-recentf-index (eh-ivy-return-recentf-index y)))
+            (if (and x-recentf-index y-recentf-index)
+              ;; Directories is sorted by `recentf-list' index
+              (< x-recentf-index y-recentf-index)
+              (string< x y)))
+          t)
+        (if (file-directory-p y)
+          nil
+          ;; Files is sorted by mtime
+          (time-less-p y-mtime x-mtime)))))
+
+  (add-to-list 'ivy-sort-functions-alist
+    '(read-file-name-internal . eh-ivy-sort-file-function)))
+
+(after! smart-mode-line
+  :config
+  ;; (setq sml/theme 'atom-one-dark)
+  (setq sml/no-confirm-load-theme t)
+  (add-to-list 'sml/replacer-regexp-list '("^~/workspace/" " :WP: ") t)
+  (sml/setup))
+
+(after! super-save
+  :config
+  (setq super-save-auto-save-when-idle t)
+  (add-to-list 'super-save-hook-triggers 'find-file-hook)
+  (super-save-mode +1))
+
+(after! wakatime-mode
+  :custom (wakatime-cli-path "~/.pyenv/shims/wakatime")
+  :config (global-wakatime-mode))
+
