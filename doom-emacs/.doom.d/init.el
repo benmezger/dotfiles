acbc0b37a36f11c30681f9b6f212b6c98213b25f
Ben Mezger <me@benmezger.nl>
2020-02-05 16:25:56 -0300

    Emacs-doom: Disable pretty-code

--- doom-emacs/.doom.d/init.el
+++ doom-emacs/.doom.d/init.el
@@ -32,7 +32,7 @@
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
-       pretty-code       ; replace bits of code with pretty symbols
+       ;pretty-code       ; replace bits of code with pretty symbols
        ;;tabs              ; an tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
