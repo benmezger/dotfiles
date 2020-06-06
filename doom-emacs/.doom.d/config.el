222ce342e3276cbf509435c13954b7cac284b5e2
Ben Mezger <me@benmezger.nl>
2020-06-06 20:53:13 -0300

    Emacs: Use InconsolataDZ font and monokai theme

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -18,12 +18,12 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Fira Code" :size 14))
+(setq doom-font (font-spec :family "InconsolataDZ" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-monokai-spectrum)
 
 ;; If you intend to use org, it is recommended you change this!
 (setq org-directory "~/org/")
