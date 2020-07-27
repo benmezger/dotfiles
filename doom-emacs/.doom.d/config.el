aeb0db2516d0cf1d65b16cc020be94ec710534da
Ben Mezger <me@benmezger.nl>
2020-07-27 08:45:36 -0300

    Emacs: Use doom-vibrant theme

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -23,7 +23,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
-(setq doom-theme 'doom-monokai-spectrum)
+(setq doom-theme 'doom-vibrant)
 
 ;; If you want to change the style of line numbers, change this to `relative' or
 ;; `nil' to disable it:
