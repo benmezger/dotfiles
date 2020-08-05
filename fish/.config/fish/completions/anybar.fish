8ae6275d7c4dd711b83ba2a3d2fd3e3068252cd1
Ben Mezger <me@benmezger.nl>
2020-08-05 19:32:49 -0300

    Refactor: Remove dotfiles

--- fish/.config/fish/completions/anybar.fish
+++
@@ -1,6 +0,0 @@
-set -l styles white red orange yellow green cyan blue purple black question exclamation
-
-for style in $styles
-  echo "entry: $style"
-  complete -xc anybar -n "__fish_use_subcommand" -a "$style" -d "Set the Anybar dot to the $style style"
-end
--- zsh/.zsh/stack.zsh
+++
@@ -1,3 +0,0 @@
-DIRSTACKSIZE=8
-setopt autopushd pushdminus pushdsilent pushdtohome
-alias dh='dirs -v'
