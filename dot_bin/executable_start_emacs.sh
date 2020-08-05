a8c2be43b9600d2e28ab689b7ac12b9c6a251903
Ben Mezger <me@benmezger.nl>
2020-08-05 19:35:43 -0300

    Feat: Add dotfiles with chezmoi project structure

---
+++ dot_bin/executable_start_emacs.sh
@@ -0,0 +1,10 @@
+#!/usr/local/bin/zsh
+
+running=$(/usr/local/bin/emacsclient --eval '(daemonp)')
+
+if [ $running ]; then
+    /usr/local/bin/emacsclient -c --no-wait
+else
+    /usr/local/bin/emacs --daemon
+    /usr/local/bin/emacsclient -c --no-wait
+fi
