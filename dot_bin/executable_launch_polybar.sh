1e48e52cbf174295f6b61bd1f1a3e7511226e993
Ben Mezger <me@benmezger.nl>
2020-08-08 01:30:05 -0300

    i3: Use polybar and refactor configuration

--- dot_bin/executable_launch_polybar.sh
+++ dot_bin/executable_launch_polybar.sh
@@ -3,10 +3,12 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar main&
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
 
 echo "Polybar launched..."
