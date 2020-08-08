0100dd45d5ce453aed36abdf720c41784341d68c
Ben Mezger <me@benmezger.nl>
2020-08-08 03:29:59 -0300

    Font: Use Hack font globally

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -18,7 +18,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "InconsolataDZ" :size 14))
+(setq doom-font (font-spec :family "Hack" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
