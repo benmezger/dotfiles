d1a88b94ef26a37b7e31e73e2179675ea5fd60f2
Ben Mezger <me@benmezger.nl>
2020-08-19 18:43:42 -0300

    Zsh: Update ls aliases

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -17,26 +17,27 @@ if [[ ${PAGER} == 'less' ]]; then
     (( ! ${+LESS_TERMCAP_us} )) && export LESS_TERMCAP_us=$'\E[1;32m'   # Begins underline.
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lx='ll -XB'        # Lists sorted by extension (GNU only).
-alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-
 
 # if exa exist, alias to ls
 if (( ${+commands[exa]} )); then
     alias ls='exa'
+    alias l='ls'
     alias ll='exa -l'
     alias lll='exa -l | less'
     alias lla='exa -la'
     alias llt='exa -T'
     alias llfu='exa -bghHliS --git'
+else
+    alias l='ls -1A'         # Lists in one column, hidden files.
+    alias ll='ls -lh'        # Lists human readable sizes.
+    alias lr='ll -R'         # Lists human readable sizes, recursively.
+    alias la='ll -A'         # Lists human readable sizes, hidden files.
+    alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
+    alias lx='ll -XB'        # Lists sorted by extension (GNU only).
+    alias lk='ll -Sr'        # Lists sorted by size, largest last.
+    alias lt='ll -tr'        # Lists sorted by date, most recent last.
+    alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
+    alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
 fi
 
 alias sl='ls' # I often screw this up.
