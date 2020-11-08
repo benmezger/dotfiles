d079693867947bcd9235de1e53828e29c1eeac1a
Ben Mezger <me@benmezger.nl>
2020-11-08 01:37:25 -0300

    Emacs: Enable docker mode

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -79,7 +79,7 @@
   ;;ansible
   debugger          ; FIXME stepping through code, to help you add bugs
   ;;direnv
-  ;;docker
+  (docker +lsp)
   editorconfig      ; let someone else argue about tabs vs spaces
   ;;ein               ; tame Jupyter notebooks with emacs
   (eval +overlay)     ; run code, run (also, repls)
