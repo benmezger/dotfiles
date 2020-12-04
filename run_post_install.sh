1b8be1e0f80167bcf6b91f5310ec4385fbd01fc9
Ben Mezger <me@benmezger.nl>
2020-12-04 09:57:49 -0300

    Chore: Check if .ssh and file exists before chmod

--- run_post_install.sh
+++ run_post_install.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "SSH file chmod..."
-chmod 600 $HOME/.ssh/id_rsa
-chmod 600 $HOME/.ssh/id_rsa.pub 
-chmod 755 $HOME/.ssh
+if [ "$CI" = "1" ]; then
+    echo "Skipping post_install due to CI variable set to 1"
+    exit 0
+fi
+
+if [[ -d "$HOME/.ssh" ]]; then
+        echo "SSH file chmod..."
+
+        chmod 755 $HOME/.ssh
+        [[ ! -f $HOME/.ssh/id_rsa ]] && chmod 600 $HOME/.ssh/id_rsa
+        [[ ! -f $HOME/.ssh/id_rsa.pub ]] && chmod 600 $HOME/.ssh/id_rsa.pub 
+fi
+
+echo "Done"
