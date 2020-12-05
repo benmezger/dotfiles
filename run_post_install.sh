b3b407322ac07b11e4bbc01ad2743179e57fef44
Ben Mezger <me@benmezger.nl>
2020-12-05 00:41:35 -0300

    Fix: Check if variable CI exists or set a default

--- run_post_install.sh
+++ run_post_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ "$CI" = "1" ]; then
+if [ "${CI:-0}" = "1" ]; then
     echo "Skipping post_install due to CI variable set to 1"
     exit 0
 fi
@@ -10,8 +10,8 @@ if [[ -d "$HOME/.ssh" ]]; then
         echo "SSH file chmod..."
 
         chmod 755 $HOME/.ssh
-        [[ ! -f $HOME/.ssh/id_rsa ]] && chmod 600 $HOME/.ssh/id_rsa
-        [[ ! -f $HOME/.ssh/id_rsa.pub ]] && chmod 600 $HOME/.ssh/id_rsa.pub 
+        [[ -f $HOME/.ssh/id_rsa ]] && chmod 600 $HOME/.ssh/id_rsa
+        [[ -f $HOME/.ssh/id_rsa.pub ]] && chmod 600 $HOME/.ssh/id_rsa.pub 
 fi
 
 echo "Done"
