6a5a79e2f29278239478726e6688363de5481237
Ben Mezger <me@benmezger.nl>
2021-02-06 10:37:35 -0300

    Chore: Fixes related to shellcheck report

--- scripts/run_once_0002_install_deps.sh
+++ scripts/run_once_0002_install_deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SOURCE_DIR="${SOURCE_DIR:-$(dirname `pwd`)}"
+SOURCE_DIR="${SOURCE_DIR:-$(dirname $(pwd))}"
 
 export HOMEBREW_BUNDLE_FILE="$SOURCE_DIR/Brewfile"
 PACMAN_BUNDLE_FILE="$SOURCE_DIR/Pacfile"
@@ -10,5 +10,5 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Using $HOMEBREW_BUNDLE_FILE bundle file"
     brew bundle
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    sudo pacman -S $(< $PACMAN_BUNDLE_FILE) --noconfirm
+    sudo pacman -S "$(< "$PACMAN_BUNDLE_FILE")" --noconfirm
 fi
--- scripts/run_once_0006_set_ssh_perms.sh
+++ scripts/run_once_0006_set_ssh_perms.sh
@@ -8,8 +8,8 @@ if [[ -d "$HOME/.ssh" ]]; then
         echo "SSH file chmod..."
 
         chmod 755 "$HOME"/.ssh
-        [[ -f $HOME/.ssh/id_rsa ]] && chmod 600 "$HOME"/.ssh/id_rsa
-        [[ -f $HOME/.ssh/id_rsa.pub ]] && chmod 600 "$HOME"/.ssh/id_rsa.pub 
+        [[ -f "$HOME/.ssh/id_rsa" ]] && chmod 600 "$HOME"/.ssh/id_rsa
+        [[ -f "$HOME/.ssh/id_rsa.pub" ]] && chmod 600 "$HOME"/.ssh/id_rsa.pub 
 fi
 
 echo "Done"
