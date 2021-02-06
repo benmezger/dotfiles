7910c869c1d9ab340ffab8b5cddcffd9bb83fc8f
Ben Mezger <me@benmezger.nl>
2021-02-06 02:57:57 -0300

    Chore: Update install script

--- install.sh
+++ install.sh
@@ -11,18 +11,10 @@ echo "Installing required dependencies"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     isavailable brew || \
         curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
-    isavailable ansible || brew install ansible
+    isavailable chezmoi || brew install chezmoi
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    isavailable ansible || sudo pacman -S ansible
+    isavailable chezmoi || sudo pacman -S chezmoi
 fi
 
-echo "Installing ansible galaxies"
-ansible-galaxy install -r requirements.yml
-
-
-echo "Running ansible playbook for $OSTYPE"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    ansible-playbook -i inventory osx.yml -K
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    ansible-playbook -i inventory archlinux.yml -K
-fi
+echo "Running chezmoi for $OSTYPE"
+chezmoi -S "$(pwd)" init --verbose
