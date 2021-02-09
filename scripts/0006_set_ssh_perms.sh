57386b63894aaf3929fbc69adf153412fb2b6b8f
Ben Mezger <me@benmezger.nl>
2021-02-09 09:32:46 -0300

    Fix: Remove redundant scripts dir to SOURCE_DIR

--- scripts/0006_set_ssh_perms.sh
+++ scripts/0006_set_ssh_perms.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 
 SOURCE_DIR="${SOURCE_DIR:-"$(pwd)"}"
-. "$SOURCE_DIR/scripts/buildcheck.sh"
+. "$SOURCE_DIR/buildcheck.sh"
 
 if [[ -d "$HOME/.ssh" ]]; then
-        echo "SSH file chmod..."
+	echo "SSH file chmod..."
 
-        chmod 755 "$HOME"/.ssh
-        [[ -f "$HOME/.ssh/id_rsa" ]] && chmod 600 "$HOME"/.ssh/id_rsa
-        [[ -f "$HOME/.ssh/id_rsa.pub" ]] && chmod 600 "$HOME"/.ssh/id_rsa.pub 
+	chmod 755 "$HOME"/.ssh
+	[[ -f "$HOME/.ssh/id_rsa" ]] && chmod 600 "$HOME"/.ssh/id_rsa
+	[[ -f "$HOME/.ssh/id_rsa.pub" ]] && chmod 600 "$HOME"/.ssh/id_rsa.pub
 fi
