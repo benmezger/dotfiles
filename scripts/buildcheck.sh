d1520e22baa984505028c7b32a66aaa2cdc2b832
Ben Mezger <me@benmezger.nl>
2021-02-13 16:05:38 -0300

    Chore: Source ansi on buildcheck

--- scripts/buildcheck.sh
+++ scripts/buildcheck.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# dir should be lowercase so we don't conflict with
+# script defined DIR variable
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+. "$dir/ansi"
+
 if [ "${CI:-0}" = "1" ]; then
-    echo "Skipping post_install due to CI variable set to 1"
-    exit 0
+	ansi --red "Skipping post_install due to CI variable set to 1"
+	exit 0
 fi
