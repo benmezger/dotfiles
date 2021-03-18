d8edcf73e6f1682c0e8f45e83fe6129f9b2886b9
Ben Mezger <me@benmezger.nl>
2021-03-18 17:39:49 -0300

    Chore: Install python dependencies after pyenv

--- scripts/install_pyenv.sh
+++ scripts/install_pyenv.sh
@@ -4,7 +4,21 @@ set -euo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 . "$DIR/base.sh"
 
+PYTHON_REQUIREMENTS="$DIR/requirements.txt"
+PYTHON_VERSION=3.8.5
+
+# see github.com/pyenv/pyenv/issues/1219
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	export LDFLAGS="-L/usr/local/opt/zlib/lib"
+fi
+
 PYENV_DIR="$HOME/.pyenv"
 if [ ! -d "$PYENV_DIR" ]; then
 	curl https://pyenv.run | bash
+	export PATH="$HOME/.pyenv/bin:$PATH"
+	pyenv install -s $PYTHON_VERSION
 fi
+
+eval "$(pyenv init -)"
+pyenv local $PYTHON_VERSION
+pip install --upgrade -r $PYTHON_REQUIREMENTS
