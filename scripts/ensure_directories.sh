3e5243368342b92794849aee5659ec94b8e549b1
Ben Mezger <me@benmezger.nl>
2021-04-01 19:45:12 -0300

    Scripts: Create personal and work mail directory

--- scripts/ensure_directories.sh
+++ scripts/ensure_directories.sh
@@ -6,7 +6,9 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 . "$DIR/ansi"
 
 ansi --yellow "Ensuring required directories exist.."
-mkdir -pv $HOME/mail
-mkdir -pv $HOME/mail/.attachments
 mkdir -pv $HOME/workspace/opt
 mkdir -pv $HOME/workspace/go
+
+mkdir -pv $HOME/mail/personal
+mkdir -pv $HOME/mail/work
+mkdir -pv $HOME/mail/.attachments
