6a848f3a6ce6933382a96f0813fd2e4898049b0f
Ben Mezger <me@benmezger.nl>
2021-08-21 01:36:38 -0300

    bin: add lock file helper

---
+++ dot_bin/helpers/flock.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT
+
+## Copyright (C) 2009 Przemyslaw Pawelczyk <przemoc@gmail.com>
+##
+## This script is licensed under the terms of the MIT license.
+## https://opensource.org/licenses/MIT
+#
+# Lockable script boilerplate
+
+### HEADER ###
+
+LOCKFILE="/tmp/$(basename $0)"
+LOCKFD=99
+
+# PRIVATE
+_lock() { flock -$1 $LOCKFD; }
+_no_more_locking() {
+	_lock u
+	_lock xn && rm -f $LOCKFILE
+}
+_prepare_locking() {
+	eval "exec $LOCKFD>\"$LOCKFILE\""
+	trap _no_more_locking EXIT
+}
+
+# ON START
+_prepare_locking
+
+# PUBLIC
+exlock_now() { _lock xn; } # obtain an exclusive lock immediately or fail
+exlock() { _lock x; }      # obtain an exclusive lock
+shlock() { _lock s; }      # obtain a shared lock
+unlock() { _lock u; }      # drop a lock
