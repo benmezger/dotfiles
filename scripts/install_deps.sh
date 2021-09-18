1e91c8c4e99277f8159da091063b591e383c8d6c
Ben Mezger <me@benmezger.nl>
2021-09-17 21:41:46 -0300

    scripts: move zip curls to .chezmoiexternal

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -10,26 +10,7 @@ PACMAN_BUNDLE_FILE="$DIR/Pacfile"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	ansi --green "Using $HOMEBREW_BUNDLE_FILE bundle file"
-
 	brew bundle
-
-	ansi --green "Installing Mute Me app"
-	curl https://github.com/pixel-point/mute-me/releases/download/v2.0.0-rc2/mute-me-v2.0.0-rc2.zip \
-		-L -o /tmp/muteme.zip
-
-	unzip -o /tmp/muteme.zip -d /tmp/
-	rm -rf "$HOME/Applications/Mute Me.app/"
-	mv "/tmp/Mute Me.app" "$HOME/Applications"
-
-	ansi --green "Installing Itsycal"
-	curl https://itsycal.s3.amazonaws.com/Itsycal.zip \
-		-L -o /tmp/itsycal.zip
-
-	unzip -o /tmp/itsycal.zip -d /tmp/
-	rm -rf "$HOME/Applications/Itsycal.app/"
-	mv "/tmp/Itsycal.app" "$HOME/Applications"
-
-
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	# ask sudo upfront
 	sudo -v
