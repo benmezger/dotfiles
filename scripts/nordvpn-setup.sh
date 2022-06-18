7185f3da8c8d5140ba21ee652b8612df64283134
Ben Mezger <me@benmezger.nl>
2022-06-18 12:33:50 +0200

    scripts: configure firewall and killswitch

--- scripts/nordvpn-setup.sh
+++ scripts/nordvpn-setup.sh
@@ -13,6 +13,8 @@ fi
 nordvpn set technology nordlynx
 nordvpn set cybersec enabled
 nordvpn set autoconnect enabled
+nordvpn set firewall on
+nordvpn set killswitch on
 nordvpn whitelist add port 22
 nordvpn whitelist add subnet 192.168.0.1/24
 nordvpn whitelist add subnet 192.168.0.0/1
