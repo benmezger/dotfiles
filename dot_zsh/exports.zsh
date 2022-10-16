4c9f8ece6afbc6406ed7c40b09101c45719d5853
Ben Mezger <me@benmezger.nl>
2022-10-16 22:49:31 +0200

    zsh: remove WORKON_HOME from virtualenv

--- dot_zsh/exports.zsh
+++ dot_zsh/exports.zsh
@@ -60,7 +60,6 @@ fi
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
-export WORKON_HOME=$HOME/.virtualenvs
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 . $(pyenv root)/completions/pyenv.zsh
