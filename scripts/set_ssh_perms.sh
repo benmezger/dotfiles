eef39ac788bad1a8fe7bf4d99273d706d18bcd96
Ben Mezger <me@benmezger.nl>
2022-12-31 02:02:27 +0100

    scripts: fix quotation

--- scripts/set_ssh_perms.sh
+++ scripts/set_ssh_perms.sh
@@ -11,6 +11,6 @@ if [[ -d "$HOME/.ssh" ]]; then
 	ansi --green "SSH file chmod..."
 
 	chmod 755 "$HOME"/.ssh
-	[[ -f "$HOME/.ssh/id_rsa" ]] && chmod 600 "$HOME"/.ssh/id_rsa
-	[[ -f "$HOME/.ssh/id_rsa.pub" ]] && chmod 600 "$HOME"/.ssh/id_rsa.pub
+	[[ -f "$HOME/.ssh/id_rsa" ]] && chmod 600 "$HOME/.ssh/id_rsa"
+	[[ -f "$HOME/.ssh/id_rsa.pub" ]] && chmod 600 "$HOME/.ssh/id_rsa.pub"
 fi
