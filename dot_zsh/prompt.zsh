e0ff1c0bfdd6d07fbea59bd6eaf2c503622ce695
Ben Mezger <me@benmezger.nl>
2023-04-23 01:29:19 +0200

    zsh: add prompt spacing

--- dot_zsh/prompt.zsh
+++ dot_zsh/prompt.zsh
@@ -12,12 +12,12 @@ ZSH_THEME_GIT_PROMPT_UPSTREAM_PREFIX="%{$fg[red]%}(%{$fg[yellow]%}"
 ZSH_THEME_GIT_PROMPT_UPSTREAM_SUFFIX="%{$fg[red]%})"
 ZSH_THEME_GIT_PROMPT_BEHIND="↓"
 ZSH_THEME_GIT_PROMPT_AHEAD="↑"
-ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[red]%}✖"
-ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[green]%}●"
-ZSH_THEME_GIT_PROMPT_UNSTAGED="%{$fg[red]%}✚"
+ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[red]%}✖ "
+ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[green]%}● "
+ZSH_THEME_GIT_PROMPT_UNSTAGED="%{$fg[red]%}✚ "
 ZSH_THEME_GIT_PROMPT_UNTRACKED="…"
-ZSH_THEME_GIT_PROMPT_STASHED="%{$fg[blue]%}⚑"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✔"
+ZSH_THEME_GIT_PROMPT_STASHED="%{$fg[blue]%}⚑ "
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✔ "
 
 PROMPT='%B%40<..<%~%b$(gitprompt)'
 PROMPT+='%(?.%(!.%F{yellow}.%F{grey}).%F{red}) $%f '
