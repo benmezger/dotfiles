94a03b70d63f45bb6b429b5886d345b33eea694b
Ben Mezger <me@benmezger.nl>
2024-12-25 21:36:50 +0100

    chore: prepare repository for nix

--- run_on_change-chezmoi.sh
+++ run_on_change-chezmoi.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 
-SOURCE_DIR=$(chezmoi source-path)
-export SOURCE_DIR
+echo "Fixing GnuPG permissions"
+chown -R "$USER" ~/.gnupg/
+chmod 700 ~/.gnupg/*
+chmod 700 ~/.gnupg
 
-(cd $SOURCE_DIR && make fix-permissions)
+chmod 755 "$HOME/.ssh"
+[ -f "$HOME/.ssh/id_ed25519" ] && chmod 600 "$HOME/.ssh/id_ed25519"
+[ -f "$HOME/.ssh/id_ed25519.pub" ] && chmod 600 "$HOME/.ssh/id_ed25519.pub"
