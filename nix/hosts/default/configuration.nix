7434fb41e5cddab58d71862297bd38d2627c9974
Ben Mezger <me@benmezger.nl>
2024-12-25 16:51:51 +0100

    nix: Add fonts

--- nix/hosts/default/configuration.nix
+++ nix/hosts/default/configuration.nix
@@ -105,6 +105,13 @@
 
   services.locate.enable = true;
 
+  fonts.packages = [
+    pkgs.nerd-fonts.hack
+    pkgs.font-awesome
+  ];
+
+  fonts.fontconfig.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
