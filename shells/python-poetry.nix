793dd60affdfa9e13c6a232e3a48c6e26c14e79c
Ben Mezger <me@benmezger.nl>
2025-01-19 15:40:12 +0100

    nix: add library path to poetry

--- shells/python-poetry.nix
+++ shells/python-poetry.nix
@@ -4,15 +4,22 @@
 }:
 
 let
+  inherit (pkgs) lib stdenv;
   pythonPackage = builtins.getAttr (python) pkgs;
   poetry = pkgs.poetry.override { python3 = pythonPackage; };
 in
 pkgs.mkShell {
+  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+    stdenv.cc.cc
+    pkgs.zlib # numpy
+  ];
+  NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
   buildInputs = [
     pythonPackage
     poetry
   ];
   shellHook = ''
+    export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
     export POETRY_HOME=${poetry}
     export POETRY_VIRTUALENVS_IN_PROJECT=true
   '';
