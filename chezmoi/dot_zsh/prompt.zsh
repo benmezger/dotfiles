e9195a688edd347529d10f8daf120d1440cb1db0
Ben Mezger <me@benmezger.nl>
2025-01-19 17:36:39 +0100

    zsh: prefix [nix-shell] when in inside a shell env

--- chezmoi/dot_zsh/prompt.zsh
+++ chezmoi/dot_zsh/prompt.zsh
@@ -22,3 +22,28 @@ ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✔ "
 PROMPT='%B%40<..<%~%b$(gitprompt)'
 PROMPT+='%(?.%(!.%F{yellow}.%F{grey}).%F{red}) $%f '
 RPROMPT=''
+
+function prompt_nix_shell_precmd {
+  if [[ -n ${IN_NIX_SHELL} && ${IN_NIX_SHELL} != "0" || ${IN_NIX_RUN} && ${IN_NIX_RUN} != "0" ]]; then
+    if [[ -n ${IN_WHICH_NIX_SHELL} ]] then
+      NIX_SHELL_NAME=": ${IN_WHICH_NIX_SHELL}"
+    fi
+    if [[ -n ${IN_NIX_SHELL} && ${IN_NIX_SHELL} != "0" ]]; then
+      NAME="nix-shell"
+    else
+      NAME="nix-run"
+    fi
+    NIX_PROMPT="%F{8}[%F{3}${NAME}${NIX_SHELL_NAME}%F{8}]%f"
+    if [[ $PROMPT != *"$NIX_PROMPT"* ]] then
+      PROMPT="$NIX_PROMPT $PROMPT"
+    fi
+  fi
+}
+
+function prompt_nix_shell_setup {
+  autoload -Uz add-zsh-hook
+  add-zsh-hook precmd prompt_nix_shell_precmd
+}
+
+# Enable prompt prefixed with `[nix-shell]` when in a nix-shell environment.
+prompt_nix_shell_setup "$@"
