be465a0dc791664db4044fe1c7e0e8bbdf8280b0
Ben Mezger <me@benmezger.nl>
2025-01-25 23:41:50 +0100

    nix: enable docker

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -107,6 +107,16 @@
     glibc
   ];
 
+  virtualisation.docker = {
+    enable = true;
+    storageDriver = "btrfs";
+    enableOnBoot = false;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
