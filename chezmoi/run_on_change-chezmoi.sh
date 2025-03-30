ef5cf7dfecd02be48686b1b5080c520b1b801b28
Ben Mezger <me@benmezger.nl>
2025-03-30 01:01:31 +0100

    chezmoi: refresh gpg keys after applying

--- chezmoi/run_on_change-chezmoi.sh
+++ chezmoi/run_on_change-chezmoi.sh
@@ -5,6 +5,9 @@ chown -R "$USER" ~/.gnupg/
 chmod 700 ~/.gnupg/*
 chmod 700 ~/.gnupg
 
+echo "Refreshing GPG keys"
+gpg --refresh-keys
+
 chmod 755 "$HOME/.ssh"
 [ -f "$HOME/.ssh/id_ed25519" ] && chmod 600 "$HOME/.ssh/id_ed25519"
 [ -f "$HOME/.ssh/id_ed25519.pub" ] && chmod 600 "$HOME/.ssh/id_ed25519.pub"
