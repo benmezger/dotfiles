8a5d08d491ccb95c95e038d74dcc947347059293
Ben Mezger <me@benmezger.nl>
2025-08-30 23:35:37 +0200

    chezmoi: ensure we update doom modules

--- chezmoi/run_on_change-chezmoi.sh
+++ chezmoi/run_on_change-chezmoi.sh
@@ -11,3 +11,6 @@ gpg --refresh-keys
 chmod 755 "$HOME/.ssh"
 [ -f "$HOME/.ssh/id_ed25519" ] && chmod 600 "$HOME/.ssh/id_ed25519"
 [ -f "$HOME/.ssh/id_ed25519.pub" ] && chmod 600 "$HOME/.ssh/id_ed25519.pub"
+
+echo "Updating doom modules"
+doom sync -e
