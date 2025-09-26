2f3ef3d44dec8ac99024386a6f02d91f9bb3b51e
Ben Mezger <me@benmezger.nl>
2025-09-27 00:50:26 +0200

    zsh: remove nix linux config

--- chezmoi/dot_zsh/linux.zsh
+++ chezmoi/dot_zsh/linux.zsh
@@ -1,31 +1,11 @@
 #!/usr/bin/env zsh
 
-export PATH="$HOME/.nix-profile/bin:/run/current-system/sw/bin:$PATH"
+source /usr/share/fzf/completion.zsh
+source /usr/share/fzf/key-bindings.zsh
+source /usr/share/zsh-antidote/antidote.zsh
+source $(pyenv root)/completions/pyenv.zsh
 
-if (( $+commands[nix-env] )); then
-	export NIX_PATH="nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
-	export NIX_PATH="$NIX_PATH:nixos-config=$HOME/workspace/nix/hosts/default/configuration.nix"
-	export PATH="$HOME/.nix-profile/bin:/run/current-system/sw/bin:$PATH"
-	export PATH="/run/wrappers/bin/:$PATH:/bin"
-
-	source $HOME/.nix-profile/share/fzf/completion.zsh
-	source $HOME/.nix-profile/share/fzf/key-bindings.zsh
-
-	export ZSH_WAKATIME_BIN="$HOME/.nix-profile/bin/wakatime-cli"
-
-	source $HOME/.nix-profile/share/antidote/antidote.zsh
-	source $HOME/.nix-profile/share/zsh/site-functions/_pyenv
-	source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
-else
-	source /usr/share/fzf/completion.zsh
-	source /usr/share/fzf/key-bindings.zsh
-
-	export ZSH_WAKATIME_BIN="/usr/bin/wakatime"
-
-	source /usr/share/zsh-antidote/antidote.zsh
-
-	source $(pyenv root)/completions/pyenv.zsh
-fi
+export ZSH_WAKATIME_BIN="/usr/bin/wakatime"
 
 # docker
 export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
