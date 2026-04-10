5ccc99df4551add440c771402a2d2a9f8e94c747
Ben Mezger <me@benmezger.nl>
2026-04-10 16:32:19 +0200

    emacs: move straight.el to early-init.el

--- chezmoi/dot_emacs.d/early-init.el
+++ chezmoi/dot_emacs.d/early-init.el
@@ -2,3 +2,19 @@
 
 ;; Prevent package.el from loading since we use straight.el.
 (setq package-enable-at-startup nil)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
--- chezmoi/dot_emacs.d/init.el
+++ chezmoi/dot_emacs.d/init.el
@@ -1,18 +1,3 @@
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
 
 (load (expand-file-name "chezmoi.el" user-emacs-directory) t)
 
